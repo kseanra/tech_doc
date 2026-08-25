9e52838d1d5f5cab79694acbe39cb3ab2cd5df77
Ken Liang <kenliangnz@gmail.com>
2026-08-25 16:22:10 +1200

    update google analytisc

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,11 +1,12 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){ dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-HBXSY1GFYT');
+ window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YD3G42R1F1');
 
 (function(){
   var s = document.createElement('script');
   s.async = true;
-  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-HBXSY1GFYT';
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-YD3G42R1F1';
   document.head.appendChild(s);
 })();
